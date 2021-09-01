88ce826c6c045442eb5749b072f40d2f10ad3f44
siamshaeed <siamshaeed@gmail.com>
2021-09-01 15:56:08 +0600

    auth complet

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,17 @@
 <?php
 
-namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Route; 
+use App\Http\Controllers\MrouteController;
 
-use Illuminate\Support\Facades\Route;
 
 
-Route::get('/', [MrouteController::class, 'homepage'])->name('home');
+
+Route::get('/', [MrouteController::class, 'homepage'])->name('index');
 Route::get('/about', [MrouteController::class, 'about'])->name('about');
 Route::get('/contact', [MrouteController::class, 'contact'])->name('contact');
 Route::get('/pricing',[MrouteController::class, 'pricing'])->name('pricing');
 Route::get('/get_start',[MrouteController::class, 'getstart'])->name('getstarts');
+
+
+Auth::routes();
+Route::get('/home', [\App\Http\Controllers\HomeController::class, 'index'])->name('home');
